ccff9d6045bfe310fb10e82c7b3320867d589fe9
Theo Howell <sqonk@sqonk.com.au>
2020-07-26 20:03:19 +1000

    Unit tests for basic dispatching.

---
+++ tests/TaskTest.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+
+use PHPUnit\Framework\TestCase;
+use sqonk\phext\detach\Dispatcher;
+
+class TaskTest extends TestCase
+{
+    public function testResult()
+    {
+        detach (function() {
+            return 100;
+        });
+        $results = detach_wait();
+        $this->assertEquals(100, $results[0]);
+    }
+    
+    protected function dispatch($amount)
+    {
+        $input = range(1, $amount);
+
+        foreach ($input as $i)
+        {
+            detach (function() use ($i) {
+                usleep(rand(100, 1000));
+            	return [$i, $i+5];
+            });
+        }
+        
+        $results = detach_wait();
+        $this->assertSame(count($input), count($results));
+        
+        foreach ($results as $r)
+            $this->assertEquals($r[0]+5, $r[1]);
+    }
+    
+    public function testDispatch10Tasks()
+    {
+        $this->dispatch(10);
+    }
+    
+    public function testDispatch100Tasks()
+    {
+        $this->dispatch(100);
+    }
+    
+    public function testWaitAny()
+    {
+        detach (function() {
+            return 1;
+        });
+        detach (function() {
+            return 2;
+        });
+        
+        $this->assertContains(Dispatcher::wait_any(), [1,2]);
+        detach_wait(); // clear out the other result.
+    }
+    
+    public function testWaitSingle()
+    {
+        $t = detach(function() {
+            return 1;
+        });
+        $r = detach_wait($t);
+        return $this->assertSame(1, $r);
+    }
+    
+    public function testDetachWithArgsWhatWePutInIsWhatWeGetOut()
+    {
+        detach (function($a, $b) {
+            return [$a, $b]; // return what got given.
+        }, [10, 2.5]);
+        
+        $r = detach_wait();  
+        $this->assertEquals(1, count($r));
+        
+        $this->assertEquals(10, $r[0][0]);
+        $this->assertEquals(2.5, $r[0][1]);
+    }
+}
