f041d43356e16e205e1b2fb7f1749aeec340d5b6
Theo Howell <sqonk@sqonk.com.au>
2020-07-27 19:34:41 +1000

    TaskMap tests.

---
+++ tests/TaskMapTest.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+
+use PHPUnit\Framework\TestCase;
+use sqonk\phext\detach\Dispatcher;
+
+class TaskMapTest extends TestCase
+{   
+    protected function runMap($amount, bool $block, int $limit = 0)
+    {
+        $range = range(1, $amount);
+        $map = Dispatcher::map($range, function($i) {
+        	return $i;
+        });
+        if ($limit)
+            $map->limit($limit);
+        $map->block($block);
+        
+        $results = $map->start();
+        if (! $block)
+            $results = detach_wait();
+        $this->assertSame(count($range), count($results));
+        foreach ($results as $r)
+            $this->assertContains($r, $range);
+    }
+    
+    public function testBlockingNoLimitWith10()
+    {
+        $this->runMap(10, true);
+    }
+    
+    public function testBlockingNoLimitWith100()
+    {
+        $this->runMap(100, true);
+    }
+    
+    public function testBlockingLimit3With10Tasks()
+    {
+        $this->runMap(10, true, 3);
+    }
+    
+    public function testBlockingLimit3With100Tasks()
+    {
+        $this->runMap(100, true, 3);
+    }
+    
+    public function testNonBlockingNoLimitWith10Tasks()
+    {
+        $this->runMap(10, false);
+    }
+    
+    public function testNonBlockingNoLimitWith100Tasks()
+    {
+        $this->runMap(100, false);
+    }
+    
+    public function testNonBlockingLimit3With10Tasks()
+    {
+        $this->runMap(10, false, 3);
+    }
+}
