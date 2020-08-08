5b6ad84c56fa8782a9d13dc86d971d51a3822eeb
Theo Howell <sqonk@sqonk.com.au>
2020-08-08 14:35:44 +1000

    Comment header added to all tests.

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -1,6 +1,22 @@
 <?php
 declare(strict_types=1);
-
+/**
+*
+* Threading
+* 
+* @package		phext
+* @subpackage	detach
+* @version		1
+* 
+* @license		MIT see license.txt
+* @copyright	2019 Sqonk Pty Ltd.
+*
+*
+* This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
 
 use PHPUnit\Framework\TestCase;
 use sqonk\phext\detach\Dispatcher;
